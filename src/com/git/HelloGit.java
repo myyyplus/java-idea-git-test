2a8de8d7757ea0ecfda7887442b0446e016ab9dd
Myyy <1289903263@qq.com>
2019-12-24 10:10:05 +0800

    添加test工程至git

---
+++ src/com/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
